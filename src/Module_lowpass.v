13254025fee130f3f5ad7fd7d556c3ea6cc0b8d4
heberibeiro98 <hebermsribeiro@gmail.com>
2020-12-16 21:08:31 +0000

    Updated every module with information acquired in the lab

--- src/Module_lowpass.v
+++ src/Module_lowpass.v
@@ -14,7 +14,7 @@ reg [17:0] myRAM [0:64]; //Array de registos (65 registos de 18 bits cada)
 
 integer i;
 
-reg [17:0] yk;
+reg [42:0] yk;
 reg [6:0] cont1; 
 reg [6:0] cont2; 
 
@@ -29,6 +29,7 @@ localparam [2:0] //Estados
 	state2 = 3'd2;
 	state3 = 3'd3;
 	state4 = 3'd4;
+	state5 = 3'd5;
 
 always @(posedge clock)
 begin
@@ -39,7 +40,7 @@ begin
 		state <= nextstate;
 end
 
-always @(posedge clock)
+always @*
 begin
 	case(state)
 		state0:
@@ -67,10 +68,19 @@ begin
 			end
 
 		state4:
+		begin	
+			if(endatain)
+				nextstate <= state5;
+			
+			else
+				nextstate <= state0;
+		end
+		
+		state5:
 			nextstate <= state2;
 end
 
-always @(posedge clock)
+always @*
 begin
 	case(state)
 	
@@ -105,17 +115,17 @@ begin
 
 		state4:
 		begin
-			dataout <= yk;
+			dataout <= yk[29:12];
 			yk <= 17'd0;
 			cont2 <= 7'd0;
-			
-			if(endata)
-			begin
-				for(i = 0; i < 65; i = i + 1)
+		end
+		
+		state5:
+		begin	
+			for(i = 0; i < 65; i = i + 1)
 					myRAM[i+1] <= myRAM[i];
 
 				myRAM[0] <= datain;
-			end
 		end
 
 end module
---
+++ src/psdi_dsp.v
@@ -0,0 +1,91 @@
+ module psdi_dsp(
+            input  clock,
+			input  reset,
+			
+			input [7:0] switches,
+			
+			input data_en,
+			
+            output [6:0] RAM_coefs_addr,
+		    input  [7:0] RAM_coefs_dataout,
+			
+			input [17:0] right_in,
+			input [17:0] left_in,
+			
+			output signed [17:0] right_out,
+			output signed [17:0] left_out
+		     );
+
+			
+					 
+//-------------------------------------------------------------------------------
+// Audio samples are available in the positive clock edge when data_en is 1
+// 
+// A synchronous process to handle the audio stream should be as:
+// always @(posedge clock)
+// if ( reset )
+//   // do the reset actions
+// else
+//   if ( data_en )
+//   begin
+//     // do something with right_in and left_in
+//     // and generate right_out and left_out
+//   end
+//-------------------------------------------------------------------------------
+
+
+// Set the RAM read address bus to zero:
+assign RAM_coefs_addr = 7'd0;
+
+//-------------------------------------------------------------------------------
+// Implement some basic functions using  the audio stream
+wire signed [17:0] LEFT_inf, RIGHT_inf;		
+
+
+// Set sw0=1 / sw1=1 to mute left/right inputs:
+// set sw2=1 / sw3=1 to swap left and right channels
+assign LEFT_inf   = ( switches[0] ) ? 18'd0 : ( switches[2] ? right_in : left_in  );
+assign RIGHT_inf  = ( switches[1] ) ? 18'd0 : ( switches[3] ? left_in  : right_in );	
+
+
+// Example of basic signal processing operation:
+// calculate the sum signal to the : out = (left + right) / 2
+
+reg [17:0] Lr0, Rr0;
+reg [17:0] LRsum, LRdif;
+
+always @(posedge clock)
+if ( reset )
+  begin
+    Lr0 <= 18'd0;
+    Rr0 <= 18'd0;
+    LRdif <= 18'd0;
+    LRsum <= 18'd0;
+ end
+else
+  if ( data_en )  // 48 KkHz
+  begin
+    Lr0 <= LEFT_inf;   // register the inputs
+    Rr0 <= RIGHT_inf; 
+	
+	// calculate the sum and difference of left and right channels, 
+	// sign-extend the operands to 19 bits
+    LRsum <= ( $signed( {Lr0[17], Lr0} ) + $signed( {Rr0[17], Rr0} ) ) >>> 1;
+    LRdif <= ( $signed( {Lr0[17], Lr0} ) - $signed( {Rr0[17], Rr0} ) ) >>> 1;
+  end                                                            
+
+// Select output signal with the position of the slide switches:
+assign left_out   = (switches[4]) ? LEFT_inf  : ( switches[5] ? LRsum : LRdif );		 
+assign right_out  = (switches[4]) ? RIGHT_inf : ( switches[6] ? LRdif : LRsum );
+
+Module_lowpass lowpass1(
+	.clock (clock),
+	.reset (reset),
+	
+	.datain (left_in),
+	.endata (data_en),
+	
+	.dataout (left_out)
+
+endmodule
+
